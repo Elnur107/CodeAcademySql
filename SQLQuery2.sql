313069c5a04c79598729597e01350a548aff315c
AkberliElnur <126491959+Elnur107@users.noreply.github.com>
2024-01-22 11:38:58 +0400

    Add files via upload

---
+++ SQLQuery2.sql
@@ -0,0 +1,26 @@
+﻿--- 1 Fiyatı ortalama fiyatın üstünde olan ürünler
+SELECT *
+FROM Products
+WHERE UnitPrice > (SELECT AVG(UnitPrice) FROM Products);
+ 
+
+ --- 2 Ürünler tablosudaki satılan ürünlerin listesi
+SELECT *
+FROM Products
+WHERE ProductID IN (SELECT DISTINCT ProductID FROM [Order Details]);
+
+--- 3 Ürünler tablosudaki satılmayan ürünlerin listesi
+SELECT *
+FROM Products
+WHERE ProductID NOT IN (SELECT DISTINCT ProductID FROM [Order Details]);
+
+
+--- 4 Kargo şirketlerinin taşıdıkları sipariş sayıları
+SELECT ShipAddress, COUNT(ShipVia) AS TotalOrders
+FROM Orders
+GROUP BY ShipAddress;
+
+
+--- 5 Sipariş Alan Çalışanları Listeleyiniz
+SELECT EmployeeID,FirstName,LastName FROM Employees
+WHERE EmployeeID IN (SELECT DISTINCT EmployeeID FROM Orders)
